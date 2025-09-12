6238a69f74ea76b9fc00a6bf183c63fee0a7a46b
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-09-12 14:52:09 -0600

    save

--- ros/ros2_ws/src/simple_chat/setup.py
+++ ros/ros2_ws/src/simple_chat/setup.py
@@ -1,22 +1,17 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'simple_chat'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='benjamin.peterson@student.nmt.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='A simple ROS 2 chat package',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
