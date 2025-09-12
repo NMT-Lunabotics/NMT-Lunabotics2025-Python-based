2de92ec09612f89e83961eacf6c53a2c0aa123cd
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-09-11 19:18:25 -0600

    s

--- ros/ros2_ws/build.sh
+++ ros/ros2_ws/build.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-REPO_URL="https://github.com/NMT-Lunabotics/NMT-Lunabotics2025-Python-based"
-BRANCH="benjamintestbrance
+echo '#!/bin/bash
+REPO_URL="git@github.com:NMT-Lunabotics/NMT-Lunabotics2025-Python-based.git"
+BRANCH="benjaminstestbranch"
 
 if colcon build; then
     echo "Build successful! Pushing to GitHub..."
     git add .
-    git commit -m "Auto-commit: $(date "+%Y-%m-%d %H:%M:%S")"
+    git commit -m "Auto-commit: $(date \"+%Y-%m-%d %H:%M:%S\")"
     git push $REPO_URL $BRANCH
     echo "Done! Pushed to GitHub"
 else
     echo "Build failed! Not pushing"
     exit 1
 fi' > /usr/local/bin/buildpush
-
-chmod +x /usr/local/bin/buildpush
