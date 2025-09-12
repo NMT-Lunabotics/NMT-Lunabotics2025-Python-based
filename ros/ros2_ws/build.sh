2361122d7d2b35afb246146a5c2a2d6a57dc4ef1
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-09-12 13:43:51 -0600

    save

--- ros/ros2_ws/build.sh
+++ ros/ros2_ws/build.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 set -e
 
-# Go to workspace
-cd /ros2_ws
+WORKDIR="/ros2_ws"
 
-# Source ROS 2
-source /opt/ros/humble/setup.bash
+# Default: do everything
+DO_SYNC_ONLY=false
 
-# Git setup (first time only)
-git config user.email "benjamin.peterson@student.nmt.edu"
-git config user.name "benjamin-p15"
+# Parse flags
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -s|--sync) DO_SYNC_ONLY=true ;;
+        *) echo "Unknown flag: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Git setup
+git config --global user.email "benjamin.peterson@student.nmt.edu"
+git config --global user.name "benjamin-p15"
 git remote add origin git@github.com:NMT-Lunabotics/NMT-Lunabotics2025-Python-based.git 2>/dev/null || true
+
+cd "$WORKDIR"
 
 # Sync branch
 git fetch origin
 git checkout -B benjaminstestbranch origin/benjaminstestbranch
+git merge origin/benjaminstestbranch || echo "Already up to date"
+
+# If only syncing, exit here
+if [ "$DO_SYNC_ONLY" = true ]; then
+    echo "Sync complete. Exiting."
+    exit 0
+fi
+
+# Source ROS 2
+source /opt/ros/humble/setup.bash
 
 # Build workspace
 colcon build --symlink-install
