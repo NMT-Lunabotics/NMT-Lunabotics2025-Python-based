83e89bb71707601486fd6e209f7aa832ea8a1c4e
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-09-12 11:08:04 -0600

    save

--- ros/ros2_ws/build.sh
+++ ros/ros2_ws/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-cd ~/ros2_ws
+# Where you want the repo
+WORKDIR=~/ros2_ws
 
+# Remove old repo if needed
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+
+# Clone the repo
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "Error: GITHUB_TOKEN is not set."
   exit 1
 fi
 
-# Set Git username/email for commits
-git config user.name "benjamin-p15"
-git config user.email "benjamin.peterson@student.nmt.edu"
+git clone -b benjaminstestbranch https://benjamin-p15:$GITHUB_TOKEN@github.com/NMT-Lunabotics/NMT-Lunabotics2025-Python-based.git $WORKDIR
 
-git remote set-url origin https://$GITHUB_TOKEN@github.com/NMT-Lunabotics/NMT-Lunabotics2025-Python-based.git
-
-git fetch origin benjaminstestbranch
-git checkout -B benjaminstestbranch origin/benjaminstestbranch
-git pull origin benjaminstestbranch
+cd $WORKDIR
 
 # Build project
 if [ -f Makefile ]; then
@@ -29,6 +29,9 @@ else
   echo "No known build system found."
 fi
 
+# Commit & push changes
+git config user.name "benjamin-p15"
+git config user.email "benjamin.peterson@student.nmt.edu"
 git add .
 git commit -m "Auto update from container" || true
 git push origin benjaminstestbranch
