15cec6b189a7a6fcaa0eeeccdbf44349173db6d9
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-09-15 00:50:09 -0600

    cli library

---
+++ system_control/component_tests/arduino_cli/main.py
@@ -0,0 +1,22 @@
+import subprocess
+import time
+
+sketch_path = "/home/masterpi/NMT-Lunabotics2025-Python-based/system_control/component_tests/serial_connection/usb_serial/recieve_message"
+port = "/dev/ttyACM0"
+
+# Compile the sketch
+subprocess.run(
+    f"arduino-cli compile --fqbn arduino:avr:uno {sketch_path}",
+    shell=True,
+    check=True
+)
+
+# Short delay to ensure the board is ready
+time.sleep(2)
+
+# Upload the sketch
+subprocess.run(
+    f"arduino-cli upload -p {port} --fqbn arduino:avr:uno --verbose {sketch_path}",
+    shell=True,
+    check=True
+)
--- system_control/component_tests/serial_connection/usb_serial/recieve_message/recieve_message.ino
+++ system_control/component_tests/serial_connection/usb_serial/recieve_message/recieve_message.ino
@@ -6,6 +6,7 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(ledPin, HIGH);
   if (Serial.available()) {
     String msg = Serial.readStringUntil('\n');
     msg.trim();  // remove whitespace or newline characters
