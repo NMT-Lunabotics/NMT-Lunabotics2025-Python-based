1abc4775d8d49e513ac9f3f5325215d3b31459eb
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-09-28 23:29:30 -0600

    build file should work with ignore list

--- build.sh
+++ build.sh
@@ -10,16 +10,20 @@ git remote add origin git@github.com:NMT-Lunabotics/NMT-Lunabotics2025-Python-ba
 
 # Git pull and overwrite local files, excluding specified files defined in ./start_docker.sh
 git fetch origin
-git sparse-checkout init --cone
+git sparse-checkout init --no-cone
 ALL_FILES=$(git ls-tree -r --name-only origin/main)
 INCLUDE=()
-for f in $ALL_FILES; do
+while IFS= read -r f; do
     skip=false
     for excl in "${EXCLUDE_FROM_PULL[@]}"; do
-        [[ "$f" == "$excl"* ]] && skip=true && break
+        [[ "$f" == "$excl"* || "$f" == "$excl" ]] && skip=true && break
     done
-    $skip || INCLUDE+=("$f")
-done
+    if [ "$skip" = false ]; then
+        INCLUDE+=("$f")
+    fi
+done <<< "$ALL_FILES"
 git sparse-checkout set "${INCLUDE[@]}"
 git reset --hard origin/main
 git clean -fdx
+RESTART_CONTAINER=true
+BUILD_IMAGE=true
