b90a4d14d7540fb1ffc85e41a02d142d9a0fa624
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-16 12:59:09 -0600

    added max motor speed

--- system_operations/system_control/system_control.ino
+++ system_operations/system_control/system_control.ino
@@ -232,8 +232,8 @@ OutPin motor_enable(EN_PIN);
 Motor motor_left(motor_left_dac1, motor_left_dac2, motor_enable, motor_max_vel, false);
 Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable, motor_max_vel, true);
 #else
-SimpleMotor simpleMotorLeft(3, 4, 9);
-SimpleMotor simpleMotorRight(6, 7, 8);
+SimpleMotor simpleMotorLeft(3, 4, 9, motor_max_vel);
+SimpleMotor simpleMotorRight(6, 7, 8, motor_max_vel);
 #endif
 #endif
 
