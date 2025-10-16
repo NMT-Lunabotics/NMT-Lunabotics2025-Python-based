ac007b033a53e8d9bf3d7b99982f964d129a954d
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-16 12:06:44 -0600

    s

--- system_operations/system_control/system_control.ino
+++ system_operations/system_control/system_control.ino
@@ -30,8 +30,8 @@
 #define SERVO_MOTOR_ENABLED          0
 #define IMU_SENSOR_ENABLED           0
 #define IBUS_REVIVER_ENABLED         0
-#define SERIAL_COMM_TIMEOUT_FAULT    0
-#define COMPONENT_TIMEOUT_FAULTS     0
+#define SERIAL_COMM_TIMEOUT_FAULT    1
+#define COMPONENT_TIMEOUT_FAULTS     1
 #define DEBUG_MODE                   0
 #define SENSOR_OUTPUT                0  
 #endif
@@ -246,13 +246,13 @@ Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable, motor_max_ve
 #else
 OutPin motor_left_dac1(DACL1_PIN);
 OutPin motor_left_dac2(DACL2_PIN);
-//OutPin motor_right_dac1(DACR1_PIN);
-//OutPin motor_right_dac2(DACR2_PIN);
+OutPin motor_right_dac1(DACR1_PIN);
+OutPin motor_right_dac2(DACR2_PIN);
 OutPin motor_enable1(EN_PIN1);
-//OutPin motor_enable2(EN_PIN2);
+OutPin motor_enable2(EN_PIN2);
 
 Motor motor_left(motor_left_dac1, motor_left_dac2, motor_enable1, motor_max_vel, false);
-//Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable2, motor_max_vel, true);
+Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable2, motor_max_vel, true);
 
 #endif
 #endif
