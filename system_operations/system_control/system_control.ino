60acbad6a0c7236c3f6763095c687bb224a2edea
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-16 12:14:16 -0600

    s

--- system_operations/system_control/system_control.ino
+++ system_operations/system_control/system_control.ino
@@ -112,8 +112,8 @@ const int EN_PIN = 32;  // Common for both motors
 #else
 // Left motor
 const int DACL1_PIN = 4;  // A1
-const int DACL2_PIN = 5;  // B1
-const int EN_PIN1   = 6;  // P1
+const int DACL2_PIN = 6;  // B1
+const int EN_PIN1   = 5;  // P1
 
 // Right motor
 const int DACR1_PIN = 7;  // A2
@@ -246,13 +246,13 @@ Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable, motor_max_ve
 #else
 OutPin motor_left_dac1(DACL1_PIN);
 OutPin motor_left_dac2(DACL2_PIN);
-OutPin motor_right_dac1(DACR1_PIN);
-OutPin motor_right_dac2(DACR2_PIN);
+//OutPin motor_right_dac1(DACR1_PIN);
+//OutPin motor_right_dac2(DACR2_PIN);
 OutPin motor_enable1(EN_PIN1);
-OutPin motor_enable2(EN_PIN2);
+//OutPin motor_enable2(EN_PIN2);
 
 Motor motor_left(motor_left_dac1, motor_left_dac2, motor_enable1, motor_max_vel, false);
-Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable2, motor_max_vel, true);
+//Motor motor_right(motor_right_dac1, motor_right_dac2, motor_enable2, motor_max_vel, true);
 
 #endif
 #endif
