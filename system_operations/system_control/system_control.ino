3a67f4da61c1bb9ab43a48ad049bac239a108bbc
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-16 21:25:41 -0600

    rotation direction wrong

--- system_operations/system_control/system_control.ino
+++ system_operations/system_control/system_control.ino
@@ -579,7 +579,7 @@ void processMessage(byte *data, int length) {
         mLR_rotation_speed = (int8_t)data[1]; 
         if(mLR_rotation_speed>0&&mLR_rotation_speed>motor_max_vel) mLR_rotation_speed=motor_max_vel;
         else if(mLR_rotation_speed<0&&mLR_rotation_speed<-motor_max_vel) mLR_rotation_speed=-motor_max_vel;
-        mLR_rotation = -(int8_t)data[2];
+        mLR_rotation = (int8_t)data[2];
         mLR_arc_radius = (int8_t)data[3];
         reset_IMU_local_home = (int8_t)data[4];
         update_IMU_raw_home=false;
