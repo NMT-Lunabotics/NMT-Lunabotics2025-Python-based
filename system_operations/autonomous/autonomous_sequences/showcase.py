8851a6c5ccfbada2580cc67d673a33358aa7e33c
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-18 15:36:33 -0600

    test

--- system_operations/autonomous/autonomous_sequences/showcase.py
+++ system_operations/autonomous/autonomous_sequences/showcase.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 SEQUENCE = [
     {"command": "A", "values": [-1, -1, -1, -1, 0, 0], "duration": 0.2},
     {"command": "M", "values": [10, 10], "duration": 3},
-    {"duration": 0.1},
     {"command": "M", "values": [-10, -10], "duration": 3},
     {"command": "M", "values": [8, -8], "duration": 1.5},
     {"command": "M", "values": [-8, 8], "duration": 1.5},
