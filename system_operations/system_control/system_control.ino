ebf73d671ec0a7182b90ed686c1ba13b092d72a0
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-22 18:39:16 -0600

    test

--- system_operations/system_control/system_control.ino
+++ system_operations/system_control/system_control.ino
@@ -7,8 +7,8 @@
 // List of components flags to enable/disable for testing
 #define ERROR_LEDS_ENABLED           1
 #define MOTORS_ENABLED               1
-#define BUCKET_ACTUATOR_ENABLED      1
-#define ARM_ACTUATORS_ENABLED        1
+#define BUCKET_ACTUATOR_ENABLED      0
+#define ARM_ACTUATORS_ENABLED        0
 #define SERVO_MOTOR_ENABLED          0
 #define IMU_SENSOR_ENABLED           0
 #define IBUS_RECIVER_ENABLED         1
