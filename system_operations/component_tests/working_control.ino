1f54a19a92f7250b1929895c490326cfb95bf4ff
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-24 14:17:12 -0600

    test

--- system_operations/component_tests/working_control.ino
+++ system_operations/component_tests/working_control.ino
@@ -12,8 +12,8 @@
 #define IBUS_REVIVER_ENABLED         1
 
 // List of faults to disable
-#define SERIAL_COMM_TIMEOUT_FAULT    0
-#define COMPONENT_TIMEOUT_FAULTS     0
+#define SERIAL_COMM_TIMEOUT_FAULT    1
+#define COMPONENT_TIMEOUT_FAULTS     1
 
 // Debug mode flags
 #define DEBUG_MODE                   0
@@ -381,20 +381,22 @@ void loop() {
     if (!emergency_stop) {
       #if COMPONENT_TIMEOUT_FAULTS
       // Timeout motors and actuators if a command isn't recived within the timeout period
-      if(current_time-last_motor_cmd_time>=motor_timeout&&(mR_speed!=0||mL_speed!=0||mLR_rotation_speed!=0||mLR_rotation!=0)){
-        mR_speed=0;
-        mL_speed=0;
-        mLR_rotation_speed=0;
-        mLR_rotation=0;
-        systemFault(false,"","Motors timed out, no motor command received within "+ String(motor_timeout/1000)+"s", NONE, NONE, NONE);
-      }
-       if(current_time-last_actuator_cmd_time>=actuator_timeout && (aL_speed!=0 || aR_speed !=0 || aB_speed!=0 || aLR_tgt != -1 || aB_tgt != -1 )){
-        aLR_tgt = -1;
-        aB_tgt = -1;
-        aL_speed = 0;
-        aR_speed = 0;
-        aB_speed = 0;
-        systemFault(false,"","Actuators timed out, no actuator command received within "+ String(actuator_timeout/1000)+"s", NONE, NONE, NONE);
+      if(serial_connection_established==true){
+        if(current_time-last_motor_cmd_time>=motor_timeout&&(mR_speed!=0||mL_speed!=0||mLR_rotation_speed!=0||mLR_rotation!=0)){
+          mR_speed=0;
+          mL_speed=0;
+          mLR_rotation_speed=0;
+          mLR_rotation=0;
+          systemFault(false,"","Motors timed out, no motor command received within "+ String(motor_timeout/1000)+"s", NONE, NONE, NONE);
+        }
+        if(current_time-last_actuator_cmd_time>=actuator_timeout && (aL_speed!=0 || aR_speed !=0 || aB_speed!=0 || aLR_tgt != -1 || aB_tgt != -1 )){
+          aLR_tgt = -1;
+          aB_tgt = -1;
+          aL_speed = 0;
+          aR_speed = 0;
+          aB_speed = 0;
+          systemFault(false,"","Actuators timed out, no actuator command received within "+ String(actuator_timeout/1000)+"s", NONE, NONE, NONE);
+        }
       }
       #endif
       #if ARM_ACTUATORS_ENABLED
