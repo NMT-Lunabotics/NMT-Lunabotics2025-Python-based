f23c7a8558438c5f9f63a16b8203c35ac5e2d6ac
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2025-10-24 13:21:42 -0600

    test

--- system_operations/system_control/system_control.ino
+++ system_operations/system_control/system_control.ino
@@ -4,7 +4,7 @@
 
 // List of components flags to enable/disable for testing
 #define ERROR_LEDS_ENABLED           1
-#define MOTORS_ENABLED               0
+#define MOTORS_ENABLED               1
 #define BUCKET_ACTUATOR_ENABLED      0
 #define ARM_ACTUATORS_ENABLED        0
 #define SERVO_MOTOR_ENABLED          0
@@ -17,7 +17,7 @@
 
 // Debug mode flags
 #define DEBUG_MODE                   0
-#define SENSOR_OUTPUT                3  // 1: IMU, 2: IBUS, 3: IBUS raw
+#define SENSOR_OUTPUT                0  // 1: IMU, 2: IBUS, 3: IBUS raw
 
 //--------------- Setup used classes ---------------
 
@@ -270,7 +270,7 @@ void loop() {
     #if SENSOR_OUTPUT == 2 || SENSOR_OUTPUT == 3
     if(SENSOR_OUTPUT==3) joy = ibus.getJoystick(true);
       Serial.print("RC controller inputs: ");
-      for (int i = 0; i < 7; i++) {  
+      for (int i = 0; i < 4; i++) {  
         Serial.print(joy[i]);
         Serial.print(" ");
       }
@@ -278,8 +278,8 @@ void loop() {
     #endif
     int16_t throttle = joy[0]; 
     int16_t steering = joy[1];
-    mL_speed = constrain(throttle - steering, -30, 30);
-    mR_speed = constrain(throttle + steering, -30, 30);
+    mL_speed = constrain(throttle + steering, -30, 30);
+    mR_speed = constrain(throttle - steering, -30, 30);
     aLR_tgt = -1;
     aB_tgt = -1;
     aL_speed = -joy[3];
