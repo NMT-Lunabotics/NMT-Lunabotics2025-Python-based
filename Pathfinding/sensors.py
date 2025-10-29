c3a05ff22174d007ef072d715da739c4d8269077
Nathan-Arritt <nathan.arritt@student.nmt.edu>
2025-10-29 13:38:05 -0600

    sensors.py

--- Pathfinding/sensors.py
+++ Pathfinding/sensors.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def uncertainty_add(distance, angle, sigma):
     mean = np.array([distance, angle])
-    covariance = np.diag(sigma**2)
+    covariance = np.diag(sigma ** 2)
     distance, angle = np.random.multivariate_normal(mean, covariance)
     angle = max(angle, 0)
     return [distance, angle]
@@ -12,12 +12,10 @@ def uncertainty_add(distance, angle, sigma):
 class LaserSensor:
     def __init__(self, Range, map, uncertainty):
         self.Range = Range
-        self.speed = 4
         self.sigma = np.array([uncertainty[0], uncertainty[1]])
         self.position = (0, 0)
         self.map = map
         self.W, self.H = pygame.display.get_surface().get_size()
-        self.senseObstacles = []
 
     def distance(self, obstaclePosition):
         px = (obstaclePosition[0] - self.position[0]) ** 2
@@ -28,21 +26,17 @@ class LaserSensor:
         data = []
         x1, y1 = self.position[0], self.position[1]
         for angle in np.linspace(0, 2 * math.pi, 60, False):
-            x2, y2 = (x1 + self.Range * math.cos(angle),
-                      y1 - self.Range * math.sin(angle))
+            x2, y2 = (x1 + self.Range * math.cos(angle), y1 - self.Range * math.sin(angle))
             for i in range(0, 100):
                 u = i / 100
                 x = int(x2 * u + x1 * (1 - u))
                 y = int(y2 * u + y1 * (1 - u))
                 if 0 < x < self.W and 0 < y < self.H:
                     color = self.map.get_at((x, y))
-                    if (color[0], color[1], color[2]) == (0, 0, 0):
+                    if sum(color[:3]) < 200:
                         distance = self.distance((x, y))
                         output = uncertainty_add(distance, angle, self.sigma)
                         output.append(self.position)
                         data.append(output)
                         break
-        if len(data) > 0:
-            return data
-        else:
-            return False
+        return data if len(data) > 0 else False
