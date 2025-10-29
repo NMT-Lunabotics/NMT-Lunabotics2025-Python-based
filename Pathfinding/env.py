127aec17a8049a6951b072b2794b60d7bf8e1a87
Nathan-Arritt <nathan.arritt@student.nmt.edu>
2025-10-29 13:36:15 -0600

    env.py

--- Pathfinding/env.py
+++ Pathfinding/env.py
@@ -7,18 +7,15 @@ class buildEnvironment:
         self.pointcloud = []
         self.externalMap = pygame.image.load('Map.png')
         self.maph, self.mapw = MapDimensions
-        self.MapWindowName = 'RRT path planning'
+        self.MapWindowName = 'LIDAR Simulation'
         pygame.display.set_caption(self.MapWindowName)
         self.map = pygame.display.set_mode((self.mapw, self.maph))
         self.map.blit(self.externalMap, (0, 0))
+
         # Colors
         self.black = (0, 0, 0)
-        self.grey = (70, 70, 70)
-        self.blue = (0, 0, 255)
-        self.Green = (0, 255, 0)
-        self.Yellow = (255, 255, 0)
-        self.Red = (255, 0, 0)
-        self.white = (255, 255, 255)
+        self.green = (0, 255, 0)
+        self.red = (255, 0, 0)
 
     def AD2pos(self, distance, angle, robotPosition):
         x = distance * math.cos(angle) + robotPosition[0]
@@ -36,4 +33,10 @@ class buildEnvironment:
     def show_sensorData(self):
         self.infomap = self.map.copy()
         for point in self.pointcloud:
-            self.infomap.set_at((int(point[0]), int(point[1])), (255, 0, 0))
+            self.infomap.set_at((int(point[0]), int(point[1])), self.red)
+
+    def draw_robot(self, position):
+        pygame.draw.circle(self.infomap, self.green, (int(position[0]), int(position[1])), 3)
+
+    def draw_goal(self, position):
+        pygame.draw.circle(self.infomap, (0, 0, 255), (int(position[0]), int(position[1])), 5)
