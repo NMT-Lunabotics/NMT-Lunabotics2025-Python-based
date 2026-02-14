042db50d2231c176a151e7ac7a12c97c3b0e85cf
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2026-02-14 12:06:46 -0700

    Update ownership command in start_docker.sh
    Change ownership command to use UID and GID for host directory.

--- start_docker.sh
+++ start_docker.sh
@@ -114,7 +114,7 @@ fi
 
 # Pull latest changes from GitHub if --pull flag is used
 if [ "$GITHUB_PULL" = true ]; then
-        run_cmd docker exec -u 0 $ROS_CONTAINER_ID bash "chown -R luna:luna $WORKING_DIR_CONTAINER"
+        sudo chown -R 1000:1000 "$WORKING_DIR_HOST"
         ./pull.sh $LOCAL_PULL $LOCAL_USERNAME
         RESTART_CONTAINER=true
         BUILD_IMAGE=true
