20b664a33e3897d6c718c9dc62bd0c3267e9911d
RileyMorris05 <Riley.Morris@student.nmt.edu>
2026-03-29 14:04:02 -0600

    Treying to get -p

--- start_docker.sh
+++ start_docker.sh
@@ -114,7 +114,6 @@ fi
 
 # Pull latest changes from GitHub if --pull flag is used
 if [ "$GITHUB_PULL" = true ]; then
-        run_cmd docker exec -u 0 $ROS_CONTAINER_ID bash "chown -R luna:luna $WORKING_DIR_CONTAINER"
         ./pull.sh $LOCAL_PULL $LOCAL_USERNAME
         RESTART_CONTAINER=true
         BUILD_IMAGE=true
