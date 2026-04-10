d2db2102d74268dbc4fbfa3f80aa4a1a611021df
benjamin-p15 <benjamin.peterson@student.nmt.edu>
2026-04-10 12:17:30 -0600

    push

--- system_operations/component_tests/actuators/actuator_calibrate/uploud_code.py
+++ system_operations/component_tests/actuators/actuator_calibrate/uploud_code.py
@@ -2,23 +2,28 @@
 
 #import required packages and moduals and setup serial and cli tools.
 import sys, time; from pathlib import Path
-ROOT = Path(__file__).resolve().parent.parent.parent.parent; sys.path.insert(0, str(ROOT))
+ROOT = Path(__file__).resolve().parent.parent.parent.parent.parent; sys.path.insert(0, str(ROOT))
 from system_operations.arduino_cli import arduinoConsole
 from system_operations.arduino_serial_commuication import serialCommands
 
 # Initialize arduino and serial classes with our deseried settings, and run our compile/upload function.
 #arduino = arduinoConsole(sketch_path = ROOT/"system_operations"/"remote_rc_serial"/"host_sheild_test"/"host_sheild_test.ino", board="arduino:avr:mega")
-arduino = arduinoConsole(sketch_path = ROOT/"system_operations"/"component_tests"/"actuator_calibrate"/"actuator_calibrate.ino", board="arduino:avr:mega")
+arduino = arduinoConsole(sketch_path = ROOT/"system_operations"/"component_tests"/"actuators"/"actuator_calibrate"/"actuator_calibrate.ino", board="arduino:avr:mega")
 serial = serialCommands()
 arduino.compile_and_upload()
 
+# NOTE serial feedback is blocking, you will not recive serial error messages
+#feedback = serial.read_command_feedback()
+#if feedback:
+#    for packet in feedback: 
+#        print(packet)
+
 
 while True:
-    #feedback = serial.read_command_feedback()
-    #if feedback:
-    #    for packet in feedback: 
-    #        print(packet)
-
+    #serial.send_command("S", [180])
+    #time.sleep(0.5)
+    #serial.send_command("S", [0])
+    #time.sleep(0.5)
 
     value = serial.read_serial()
     if value: print(value)
